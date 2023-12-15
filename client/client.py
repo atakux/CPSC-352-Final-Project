d6b9c76c4754eee6dfa3d55c49fc706ac25df05e
Daniel Cazarez <danc20182@gmail.com>
2023-12-15 13:54:24 -0800

    db conn

--- client/client.py
+++ client/client.py
@@ -2,7 +2,14 @@ import sqlite3
 import os
 from pwinput import pwinput
 from utils import valid_email
-path = os.getcwd()
+from pathlib import Path
+from utils import db_connection
+
+DB = "secure_purchase_order.db"
+PARENT_DIR = Path.cwd().parent
+DB_PATH = PARENT_DIR/DB
+
+conn = db_connection(db_path=DB_PATH)
 
 def main():
     try:
@@ -12,14 +19,13 @@ def main():
             pass
         if user_in == 'n':
             user_name = input("Please enter a username: ")
+            password = pwinput(prompt="Please enter a password: ")
             email = input("Please enter an email: ")
 
             if not valid_email(email):
                 raise Exception("Invalid email.")
-        
-
-
-            user_pass = pwinput(prompt="Please enter a password: ")
+            
+            
     except Exception as e:
         print(str(e))
         print("Closing program...")
--- client/utils.py
+++ client/utils.py
@@ -1,4 +1,9 @@
 import re
+import sqlite3
 
-def valid_email(email):
+def valid_email(email: str):
   return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
+
+def db_connection(db_path: str) -> sqlite3.Connection:
+    conn = sqlite3.connect(db_path)
+    return conn
