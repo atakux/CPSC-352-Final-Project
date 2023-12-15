3f1eecae8cef36c5c267ab10b562b734494b0320
Daniel Cazarez <danc20182@gmail.com>
2023-12-15 14:14:38 -0800

    add existing user check

--- client/client.py
+++ client/client.py
@@ -18,14 +18,21 @@ def main():
         if user_in == 'y':
             pass
         if user_in == 'n':
-            user_name = input("Please enter a username: ")
+            username = input("Please enter a username: ")
             password = pwinput(prompt="Please enter a password: ")
             email = input("Please enter an email: ")
 
             if not valid_email(email):
                 raise Exception("Invalid email.")
             
+            cursor = conn.cursor()
+            sql = "select * from users where username = ? or email = ?;"
+            cursor.execute(sql, (username, email))
+            result = cursor.fetchall()
             
+            if len(result) > 0:
+                raise Exception("Username or email already in use.")
+
     except Exception as e:
         print(str(e))
         print("Closing program...")
