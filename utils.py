95a443ce0dc188519d66738741b3442a695dce64
Daniel Cazarez <danc20182@gmail.com>
2023-12-17 15:22:38 -0800

    test, add strs to utils

--- utils.py
+++ utils.py
@@ -2,8 +2,10 @@ import re
 import sqlite3
 
 def valid_email(email: str):
+  """Check if email str valid"""
   return bool(re.search(r"^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", email))
 
 def db_connection(db_path: str) -> sqlite3.Connection:
+    """Create DB connection using path str"""
     conn = sqlite3.connect(db_path)
     return conn
