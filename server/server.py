aa2b65ebec069040a9688891f653aa40da1a7aff
JerichoMontecillo <jerichomontec@csu.fullerton.edu>
2023-12-17 21:19:04 -0800

    Option Handling (NOT TESTED)

--- server/server.py
+++ server/server.py
@@ -22,6 +22,17 @@ VIEW_INV = "VIEW"
 PLACE_ORDER = "ORDER"
 CHANGE_PW = "PWD"
 
+# ---------------------------------------
+import bcrypt
+from pathlib import Path
+from utils import db_connection
+DB = "store_inventory.db"
+DB_PW = "secure_purchase_order.db"
+PARENT_DIR = Path.cwd().parent
+DB_PATH = PARENT_DIR/DB
+DB_PATH_PW = PARENT_DIR/DB_PW
+# ---------------------------------------
+
 def handle_client(conn: socket.socket, addr: tuple):
         # This is where the bulk of handling responses from the client will go
         print(f"Incoming connection from: {addr}")
@@ -42,20 +53,90 @@ def handle_client(conn: socket.socket, addr: tuple):
             if decrypted_message.upper() == DISCONNECT_MESSAGE:
                 connected = False
             elif decrypted_message.upper() == VIEW_INV:
-                # TODO: implement this
+                connection = db_connection(db_path=DB_PATH)
+                cursor = connection.cursor()
+                sql = "select * from items;"
+                cursor.execute(sql)
+                items = cursor.fetchall()
+
+                for item in items:
+                    message = message + "\n" + item
+                send_encrypted(conn, message, user_public_key)
+                connection.close()
                 print("client chose to view inventory")
             elif decrypted_message.upper() == PLACE_ORDER:
-                # TODO: implement this
+                message = "What would you like to order? (Item, amount)\n"
+                send_encrypted(conn, message, user_public_key)
                 print("client chose to place order")
             elif decrypted_message.upper() == CHANGE_PW:
-                # TODO: implement this
+                message = "Please enter username, current password and new password. (Username: Current, New)\n"
+                send_encrypted(conn, message, user_public_key)
                 print("client chose to change password")
+            elif decrypted_message[0] == '(':
+                connection = db_connection(db_path=DB_PATH)
+                cursor = connection.cursor()
+                sql = "select inventory from items where name = ?;"
+                cursor.execute(sql, [decrypted_message[0]])
+                inventory = cursor.fetchall()
+                inventory = inventory - decrypted_message[1]
+                if inventory > 0:
+                    sql = "update items set inventory = ? where id = ?;"
+                    data = [inventory, decrypted_message[0]]
+                    cursor.execute(sql, data)
+                    print(f"{decrypted_message[1]} {decrypted_message[0]} subtracted from stock.")
+                    message = f"Purchasing {decrypted_message[1]} {decrypted_message[0]}"
+                else:
+                    message = "Not enough items in stock."
+                send_encrypted(conn, message, user_public_key)
+                connection.close()
+            elif type(decrypted_message) is str:
+                index = decrypted_message.find(":")
+                index2 = decrypted_message.find(',')
+                string = ""
+                for i in range(len(message[:index])):
+                    string = string + message[i]
+                name = string
+                string = ""
+                for i in range(len(decrypted_message[index2:index2]) - 1):
+                    string = string + decrypted_message[i + index + 1]
+                string.replace(" ", "")
+                bytes = string.encode(FORMAT)
+                string = ""
+
+                for i in range(len(decrypted_message[index2:]) - 1):
+                    string = string + decrypted_message[i + index2 + 1]
+                string.replace(" ", "")
+                new_pw = string.encode(FORMAT)
+                
+                connection = db_connection(db_path=DB_PATH_PW)
+                cursor = connection.cursor()
+                sql = "select password from users;"
+                passwords = [password[0] for password in cursor.execute(sql)]
+
+                for password in passwords:
+                    if bcrypt.checkpw(bytes, password.encode(FORMAT)):
+                        new_hashed_pw = bcrypt.hashpw(new_pw, bcrypt.gensalt())
+                        sql = "update users set password = ? where id = ?;"
+                        data = [new_hashed_pw, name]
+                        cursor.execute(sql, data)
+                        print(f"Password changed for {name}")
+                        message = "Password Sucessfully Changed"
+                    else:
+                        message = "Could not find password"
+                send_encrypted(conn, message, user_public_key)
+                connection.close()
 
             message = "Test"
             encrypted_message = encrypt_message(message.encode(FORMAT), user_public_key)
             conn.send(encrypted_message)
         
         conn.close()
+
+# ---------------------------------------
+def send_encrypted(conn: socket.socket, message: str, key):
+    encrypted_message = encrypt_message(message.encode(FORMAT), key)
+    conn.send(encrypted_message)
+# ---------------------------------------
 
 # All threads
 threads: List[threading.Thread] = []
